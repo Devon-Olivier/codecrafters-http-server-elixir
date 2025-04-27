91ecbfbe78b9245f269613f0656e7cb1d9f17a08
Devon-Olivier <medevon@gmail.com>
2025-04-27 08:40:32 -0400

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -25,8 +25,12 @@ defmodule Server.HTTPListener do
   end
 
   def listen(port \\ 4221) do
+    require Logger
+
     {:ok, socket} = :gen_tcp.listen(port, [:binary, active: false, reuseaddr: true])
-    IO.puts("Server listening on port #{port}")
+
+    Logger.info("Server listening on port #{port}")
+
     accept_loop(socket)
   end
 
@@ -45,22 +49,24 @@ defmodule Server.HTTPListener do
 end
 
 defmodule Server.HTTPRequest do
+  # TODO: combine all headers into one `headers` keyword list
   @enforce_keys ~w[raw_request]a
-  defstruct ~w[
-    accept
-    accept_encoding
-    body
-    content_length
-    content_type
-    host
-    http_version
-    method
-    raw_headers
-    raw_request
-    raw_request_line
-    url
-    user_agent
-  ]a
+  defstruct [
+    :accept,
+    :content_type,
+    :host,
+    :http_version,
+    :method,
+    :raw_headers,
+    :raw_request,
+    :raw_request_line,
+    :url,
+    :user_agent,
+    accept_encoding: "",
+    body: "",
+    content_length: 0,
+    connection: "Keep-Alive"
+  ]
 
   def parse_headers(%__MODULE__{raw_headers: raw_headers} = req) do
     raw_headers
@@ -69,6 +75,7 @@ defmodule Server.HTTPRequest do
     end)
   end
 
+  # TODO: Fix - this is brittle. Header keys are case-insensitive. A possible fix is to downcase before calling and have parse_header/2 accept only lowercase strings
   defp parse_header(%__MODULE__{raw_headers: _raw_headers} = req, "Accept: " <> accept) do
     %__MODULE__{req | accept: accept}
   end
@@ -78,6 +85,13 @@ defmodule Server.HTTPRequest do
          "Accept-Encoding: " <> accept_encoding
        ) do
     %__MODULE__{req | accept_encoding: accept_encoding}
+  end
+
+  defp parse_header(
+         %__MODULE__{raw_headers: _raw_headers} = req,
+         "Connection: " <> connection
+       ) do
+    %__MODULE__{req | connection: connection}
   end
 
   defp parse_header(
@@ -111,13 +125,12 @@ defmodule Server.HTTPRequest do
       request_line_and_headers
       |> String.split("\r\n", trim: true)
 
-    [method, url, raw_http_version] =
+    [method, url, http_version] =
       raw_request_line
       |> String.split(" ", trim: true)
 
-    [_http, http_version] =
-      raw_http_version
-      |> String.split("/", trim: true)
+    method = String.upcase(method)
+    http_version = String.upcase(http_version)
 
     req
     |> then(&%__MODULE__{&1 | raw_request_line: raw_request_line})
@@ -129,6 +142,93 @@ defmodule Server.HTTPRequest do
   end
 end
 
+defmodule Server.HTTPResponse do
+  @enforce_keys ~w[status_code status_text]a
+  defstruct [
+    :status_code,
+    :status_text,
+    body: "",
+    headers: [content_length: 0, connection: "Keep-Alive"],
+    protocol: "HTTP/1.1"
+  ]
+
+  def put_header(%__MODULE__{} = res, :connection, value) do
+    do_put_header(res, :connection, value)
+  end
+
+  def put_header(%__MODULE__{} = res, :content_encoding, value) do
+    do_put_header(res, :content_encoding, value)
+  end
+
+  def put_header(%__MODULE__{} = res, :content_length, value) do
+    do_put_header(res, :content_length, value)
+  end
+
+  def put_header(%__MODULE__{} = res, :content_type, value) do
+    do_put_header(res, :content_type, value)
+  end
+
+  def put_header(%__MODULE__{} = res, :date, value) do
+    do_put_header(res, :date, value)
+  end
+
+  defp do_put_header(%__MODULE__{headers: headers} = res, key, value) do
+    new_headers = Keyword.put(headers, key, value)
+    %__MODULE__{res | headers: new_headers}
+  end
+
+  def to_string(%__MODULE__{
+        body: body,
+        headers: headers,
+        protocol: protocol,
+        status_code: status_code,
+        status_text: status_text
+      }) do
+    status_line = "#{protocol} #{status_code} #{status_text}"
+
+    head =
+      if headers == [] do
+        status_line
+      else
+        headers_string =
+          headers
+          |> Enum.map(&header_to_string/1)
+          |> Enum.join("\r\n")
+
+        """
+        #{status_line}\r
+        #{headers_string}\
+        """
+      end
+
+    """
+    #{head}\r
+    \r
+    #{body}\
+    """
+  end
+
+  defp header_to_string({:connection, value}) do
+    "Connection: #{value}"
+  end
+
+  defp header_to_string({:content_encoding, value}) do
+    "Content-Encoding: #{value}"
+  end
+
+  defp header_to_string({:content_length, value}) do
+    "Content-Length: #{value}"
+  end
+
+  defp header_to_string({:content_type, value}) do
+    "Content-Type: #{value}"
+  end
+
+  defp header_to_string({:date, value}) do
+    "Date: #{value}"
+  end
+end
+
 defmodule Server.ConnectionHandler do
   use Task
 
@@ -137,7 +237,7 @@ defmodule Server.ConnectionHandler do
   end
 
   def handle(socket) do
-    # TODO: handle errors
+    # TODO: handle errors and crashes
     case :gen_tcp.recv(socket, 0) do
       {:ok, raw_request} ->
         DynamicSupervisor.start_child(
@@ -156,7 +256,7 @@ end
 defmodule Server.RequestHandler do
   use Task
 
-  alias Server.HTTPRequest
+  alias Server.{HTTPResponse, HTTPRequest}
 
   def start_link(%{raw_request: _raw_request, socket: _socket} = request) do
     Task.start_link(__MODULE__, :handle, [request])
@@ -164,112 +264,113 @@ defmodule Server.RequestHandler do
 
   def handle(%{raw_request: raw_request, socket: socket}) do
     # TODO: handle errors
-    # {:ok, raw_request} = :gen_tcp.recv(socket, 0)
 
     req =
       %HTTPRequest{raw_request: raw_request}
       |> HTTPRequest.parse_request_line()
       |> HTTPRequest.parse_headers()
 
-    :gen_tcp.send(socket, response(req))
+    res = response(req)
+    :gen_tcp.send(socket, HTTPResponse.to_string(res))
+
+    if res.headers[:connection] == "close" do
+      :gen_tcp.close(socket)
+    end
   end
 
-  defp response(%HTTPRequest{body: _body, method: "GET", url: "/"}) do
-    """
-    HTTP/1.1 200 OK\r
-    Content-Length: 0\r
-    \r
-    """
+  defp response(%HTTPRequest{body: _body, connection: connection, method: "GET", url: "/"}) do
+    %HTTPResponse{
+      status_code: 200,
+      status_text: "OK"
+    }
+    |> HTTPResponse.put_header(:connection, connection)
   end
 
   defp response(%HTTPRequest{
+         connection: connection,
          method: "GET",
          url: "/user-agent",
          user_agent: user_agent
        }) do
-    """
-    HTTP/1.1 200 OK\r
-    Content-Type: text/plain\r
-    Content-Length: #{byte_size(user_agent)}\r
-    \r
-    #{user_agent}\
-    """
+    %HTTPResponse{
+      body: user_agent,
+      status_code: 200,
+      status_text: "OK"
+    }
+    |> HTTPResponse.put_header(:connection, connection)
+    |> HTTPResponse.put_header(:content_type, "text/plain")
+    |> HTTPResponse.put_header(:content_length, byte_size(user_agent))
   end
 
   defp response(%HTTPRequest{
          accept_encoding: accept_encoding,
-         method: "GET",
-         url: "/echo/" <> str
-       })
-       when not is_nil(accept_encoding) do
-    encodings =
-      accept_encoding
-      |> String.split(",")
-      |> MapSet.new(&String.trim/1)
-
-    if MapSet.member?(encodings, "gzip") do
-      str_gz = :zlib.gzip(str)
-
-      """
-      HTTP/1.1 200 OK\r
-      Content-Encoding: gzip\r
-      Content-Type: text/plain\r
-      Content-Length: #{byte_size(str_gz)}\r
-      \r
-      #{str_gz}\
-      """
-    else
-      """
-      HTTP/1.1 200 OK\r
-      Content-Type: text/plain\r
-      Content-Length: #{byte_size(str)}\r
-      \r
-      #{str}\
-      """
-    end
-  end
-
-  defp response(%HTTPRequest{
+         connection: connection,
          method: "GET",
          url: "/echo/" <> str
        }) do
-    """
-    HTTP/1.1 200 OK\r
-    Content-Type: text/plain\r
-    Content-Length: #{byte_size(str)}\r
-    \r
-    #{str}\
-    """
+    encodings =
+      accept_encoding
+      |> String.split(",", trim: true)
+      |> MapSet.new(&String.trim/1)
+
+    res =
+      if MapSet.member?(encodings, "gzip") do
+        str_gz = :zlib.gzip(str)
+
+        %HTTPResponse{
+          body: str_gz,
+          status_code: 200,
+          status_text: "OK"
+        }
+        |> HTTPResponse.put_header(:content_length, byte_size(str_gz))
+        |> HTTPResponse.put_header(:content_encoding, "gzip")
+      else
+        %HTTPResponse{
+          body: str,
+          status_code: 200,
+          status_text: "OK"
+        }
+        |> HTTPResponse.put_header(:content_length, byte_size(str))
+      end
+
+    res
+    |> HTTPResponse.put_header(:connection, connection)
+    |> HTTPResponse.put_header(:content_type, "text/plain")
   end
 
   defp response(%HTTPRequest{
+         connection: connection,
          method: "GET",
          url: "/files/" <> filename
        }) do
     directory = Application.get_env(:codecrafters_http_server, :directory)
     file_path = Path.join(directory, filename)
 
-    case File.read(file_path) do
-      {:ok, body} ->
-        """
-        HTTP/1.1 200 OK\r
-        Content-Type: application/octet-stream\r
-        Content-Length: #{byte_size(body)}\r
-        \r
-        #{body}\
-        """
+    res =
+      case File.read(file_path) do
+        {:ok, body} ->
+          %HTTPResponse{
+            body: body,
+            status_code: 200,
+            status_text: "OK"
+          }
+          |> HTTPResponse.put_header(:content_type, "application/octet-stream")
+          |> HTTPResponse.put_header(:content_length, byte_size(body))
 
-      {:error, :enoent} ->
-        """
-        HTTP/1.1 404 Not Found\r
-        Content-Length: 0\r
-        \r
-        """
-    end
+        {:error, :enoent} ->
+          %HTTPResponse{
+            status_code: 404,
+            status_text: "Not Found"
+          }
+      end
+
+    res
+    |> HTTPResponse.put_header(:connection, connection)
   end
 
   defp response(%HTTPRequest{
          body: body,
+         connection: connection,
          method: "POST",
          url: "/files/" <> filename
        }) do
@@ -279,19 +380,19 @@ defmodule Server.RequestHandler do
     # TODO: handle errors
     File.write(file_path, body)
 
-    """
-    HTTP/1.1 201 Created\r
-    Content-Length: 0\r
-    \r
-    """
+    %HTTPResponse{
+      status_code: 201,
+      status_text: "Created"
+    }
+    |> HTTPResponse.put_header(:connection, connection)
   end
 
-  defp response(%HTTPRequest{}) do
-    """
-    HTTP/1.1 404 Not Found\r
-    Content-Length: 0\r
-    \r
-    """
+  defp response(%HTTPRequest{connection: connection}) do
+    %HTTPResponse{
+      status_code: 404,
+      status_text: "Not Found"
+    }
+    |> HTTPResponse.put_header(:connection, connection)
   end
 end
 
